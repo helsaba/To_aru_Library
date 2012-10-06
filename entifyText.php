2d089d8a8823c33ee89464a12d78e1a810243124
Certainist <ryosuke_i_628@yahoo.co.jp>
2012-10-07 03:15:03 +0900

    ライブラリ名変更・機能大幅強化

--- entifyText.php
+++ entifyText.php
@@ -1,67 +1,32 @@
 <?php
 
-//■Analyze Entities Library Ver3.4■//
+//■Entify Text Library Ver1.0■//
 //
-//ツイートのエンティティ情報をもとに置換したりリンクを振ったり出来ます。
-//replace_str変数をご自身の開発されているアプリケーションに合わせて設定してください。
-//(いちいちeval関数を使ってると処理が重くなるので)
+//Twitter上のあらゆるテキストを解析し、最適なHTMLを出力するためのライブラリです。
+//エンティティ情報がある場合はそれを忠実に再現し、
+//無い場合はライブラリ側で解析します。
+//
+//置換するURL等が完全に自分用なので、ご自分の環境に合わせて編集し、ご利用ください。
 //
 //
-//※ライブラリというより自分用化してきた気もしますが、非常に便利なのでｒｙ
 //
+
+mb_internal_encoding('UTF-8');
 
 //via.me用に設定必須
 define(VIA_ME_APP_KEY,"");
 
-//・Ver3.4
-//ow.lyに関する不具合を修正
-//簡易化関数がAPIバージョンを自動判別するように改良
-//
-//・Ver3.3.1
-//「～のツイート」に置換する部分のpreg_matchに第3引数を渡していなかったのを修正
-//
-//・Ver3.3
-//検索APIに対応・ソースコード整理
-//
-//・Ver3.2.1
-//<br>の置換タイミングに関するバグを修正
-//
-//・Ver3.2
-//via.meに対応
-//
-//・Ver3.1.1
-//media_urlのバグを修正
-//
-//・Ver3.1
-//media_urlを修正
-//ツイートURL処理と改行変換処理を移入
-//
-//・Ver3.0
-//重複しそうな関数が増えてきたのでクラス化
-//Vガラケー向けにサムネイル展開を徹底強化
-//(ファイルシーク連携)
-//
-//・Ver2.1
-//Ver2.1の不具合を修正
-//置換の方法を間違えていた
-//
-//・Ver2.0
-//別に分けていたライブラリ組み込み
-//Ver1.1の不具合を修正
-//
-//・Ver1.1
-//media_urlを展開するように修正
-//
-
 //簡易化関数
-function analyzeEntities($text,$entities) {
+function entify($text,$entities=NULL,$get_headers=false) {
 	
-	return analyzeEntitiesClass::analyze($text,$entities);
-
+	if (!is_null($entities))
+	return entifyTextClass::entifyByEntities($text,$entities,$get_headers);
+	return entifyTextClass::entifyWithoutEntities($text);
+	
 }
 
 //クラス
-class analyzeEntitiesClass {
+class entifyTextClass {
 	
 	//UID取得
 	function getUID() {
@@ -83,7 +48,8 @@ class analyzeEntitiesClass {
 		
 	}
 	
-	function analyze($text,$entities) {
+	//エンティティで解析
+	function entifyByEntities($text,$entities,$get_headers=false) {
 		
 		/* ["@attributes"]の["start"]の値を基準にソートする */
 			
@@ -180,58 +146,27 @@ class analyzeEntitiesClass {
 			
 				case "user_mention":
 				
-					/* メンションを置換するフォーマット */
-					
-					//////変更ブロックここから
+					/* メンションを置換する */
 					$replace_str = "<a href=\"person.php?guid=ON&person={$entity->screen_name}\">@{$entity->screen_name}</a>";
-					//////変更ブロックここまで
-					
 					break;
 					
 				case "url":
 				
-					/* URLを置換するフォーマット */
-					
-					//////変更ブロックここから
-					$details = self::get_image_details($entity->expanded_url);
-					//YouTubeであった場合
-					if (!is_null($details['youtube'])) 
-					$replace_str = "<br><a href=\"{$details['youtube']}\"><img src=\"{$details['thumb']}\"></a><br>";
-					//画像があり且つガラケーであった場合
-					elseif (!is_null($details['thumb']) && !is_null(self::getUID()))
-					$replace_str = "<br><a href=\"http://web.fileseek.net/getimg.cgi?guid=ON&u=".rawurlencode($details['raw'])."\"><img src=\"{$details['thumb']}\"></a><br>";
-					//画像がありガラケーでない場合
-					elseif (!is_null($details['thumb']))
-					$replace_str = "<br><a href=\"{$entity->expanded_url}\"><img src=\"{$details['thumb']}\"></a><br>";
-					//ツイートURLの正規表現に一致した場合
-					elseif (preg_match("#^https?://twitter\.com/([A-Za-z0-9_]{1,15})/status(es)?/(\d+)#",$entity->expanded_url,$matches))
-					$replace_str = "<a href=\"showTweet.php?guid=ON&id={$matches[3]}\" style=\"color:blue;\">{$matches[1]}のツイート</a>";
-					//その他
-					else
-					$replace_str = "<a href=\"{$entity->expanded_url}\">{$entity->display_url}</a>";
-					//////変更ブロックここまで
-					
+					/* URLを置換する */
+					$replace_str = self::entify_url($entity->expanded_url,$get_headers);
 					break;
 					
 				case "hashtag":
 				
-					/* ハッシュタグを置換するフォーマット */
-					
-					//////変更ブロックここから
-					$replace_str = "<a href=\"search.php?guid=ON&sq=".rawurlencode("#{$entity->text}")."\">#{$entity->text}</a>"; //この行を変更
-					//////変更ブロックここまで
-					
+					/* ハッシュタグを置換する */
+					$replace_str = "<a href=\"search.php?guid=ON&sq=".rawurlencode("#{$entity->text}")."\">#{$entity->text}</a>";
 					break;
 					
 				case "media":
 				
-					/* メディアを置換するフォーマット */
-					
-					//////変更ブロックここから
+					/* メディアを置換する */
 					$raw_url = (is_null(self::getUID())) ? $entity->media_url : "http://web.fileseek.net/getimg.cgi?guid=ON&u=".rawurlencode($entity->media_url);
 					$replace_str = "<br><a href=\"{$raw_url}\"><img src=\"{$entity->media_url}:thumb\"></a><br>";
-					//////変更ブロックここまで
-					
 					break;
 					
 			}
@@ -249,6 +184,51 @@ class analyzeEntitiesClass {
 		
 		//処理したテキストを返す
 		return $text;
+		
+	}
+	
+	//エンティティを使わずに解析
+	function entifyWithoutEntities($text) {
+		
+		//解析された配列に変換
+		$array = self::__toArray($text);
+		
+		$new_text = "";
+		
+		//文節ごとに処理
+		foreach ($array as $element) {
+		
+			switch ($element['type']) {
+			
+			//通常
+			case false:
+			case 'word':
+				$new_text .= $element['str'];
+				break;
+			
+			//URL
+			case 'url':
+				$parsed = parse_url($element['str']);
+				$parsed['scheme'] = ($parsed['scheme']) ? $parsed['scheme']."://" : "http://";
+				$url = $parsed['scheme'].$parsed['host'].$parsed['path'].$parsed['query'].$parsed['fragment'];
+				$new_text .= self::entify_url($url);
+				break;
+			
+			//スクリーンネーム
+			case 'screen_name':
+				$new_text .= "<a href=\"person.php?guid=ON&person={$element['str']}\">{$element['str']}</a>";
+				break;
+				
+			//ハッシュタグ
+			case 'hashtag':
+				$new_text .= "<a href=\"search.php?guid=ON&sq=".rawurlencode($element['str'])."\">{$element['str']}</a>";
+			
+			}
+			
+		}
+		
+		//処理済みの文字列を返す
+		return $new_text;
 		
 	}
 	
@@ -331,9 +311,9 @@ class analyzeEntitiesClass {
 			$ow_jpg = 'http://static.ow.ly/photos/original/'.$matches[1].'.jpg';
 			$ow_png = 'http://static.ow.ly/photos/original/'.$matches[1].'.png';
 			$ow_gif = 'http://static.ow.ly/photos/original/'.$matches[1].'.gif';
-			if (self::ow_exist($ow_jpg)) $img_raw = $ow_jpg;
-			elseif (self::ow_exist($ow_png)) $img_raw = $ow_png;
-			elseif (self::ow_exist($ow_gif)) $img_raw = $ow_gif;
+			if (self::url_exists($ow_jpg)) $img_raw = $ow_jpg;
+			elseif (self::url_exists($ow_png)) $img_raw = $ow_png;
+			elseif (self::url_exists($ow_gif)) $img_raw = $ow_gif;
 			
 			//該当しなければサムネイルを適用
 			else $img_raw = $img_thumb;
@@ -378,10 +358,193 @@ class analyzeEntitiesClass {
 		
 	}
 	
-	// ow.ly用
-	private function ow_exist($ow_url) {
-		$ow_get = @simplexml_load_string(@file_get_contents($ow_url));
-		if ($ow_get===false) return true; else return false;
+	//指定されたURLが存在するかどうかを確認する
+	function url_exists($url) {
+	
+		$headers = get_headers($url);
+		//403か404エラー以外はOKと見なす
+		if (mb_strpos($headers[0],"403")!==false || mb_strpos($headers[0],"404")!==false)
+		return false;
+		else
+		return true;
+		
+	}
+	
+	//(短縮)URLを解析してHTMLエンティティを付加したものを返す
+	function entify_url($url,$get_headers=false){
+		
+		//画像URLパターンと照合
+		$details = self::get_image_details($url);
+		
+		//YouTubeであった場合
+		if (!is_null($details['youtube'])) 
+		return "<br><a href=\"{$details['youtube']}\"><img src=\"{$details['thumb']}\"></a><br>";
+		
+		//画像があり且つガラケーである場合
+		elseif (!is_null($details['thumb']) && !is_null(self::getUID()))
+		return "<br><a href=\"http://web.fileseek.net/getimg.cgi?guid=ON&u=".rawurlencode($details['raw'])."\"><img src=\"{$details['thumb']}\"></a><br>";
+		
+		//画像がありガラケーでない場合
+		elseif (!is_null($details['thumb']))
+		return "<br><a href=\"{$url}\"><img src=\"{$details['thumb']}\"></a><br>";
+		
+		//ツイートURL(またはそのメディア)の正規表現に一致した場合
+		elseif (preg_match("#^https?://twitter\.com/([A-Za-z0-9_]{1,15})/status(es)?/(\d+)#",$url,$matches))
+		return "<a href=\"showTweet.php?guid=ON&id={$matches[3]}\" style=\"color:blue;\">{$matches[1]}のツイート</a>";
+		
+		//ヘッダー取得が無効の場合停止してクッションページ用URLを適用
+		elseif ($get_headers===false)
+		return self::make_jump_url($url);
+		
+		//いずれにも一致しなかったときさらにヘッダー取得
+		$headers = get_headers($url);
+		
+		//取得に失敗したら停止してクッションページ用URLを適用
+		if ($headers===false)
+		return self::make_jump_url($url);
+		
+		//逆順に並び替えてLocationがあれば再帰させる
+		$headers = array_reverse($headers);
+		foreach ($headers as $h) {
+			if (strpos($h,"Location: http")!==0) continue;
+			return self::entify_url(substr($h,10),true);
+		}
+		
+		//それ以上Locationが見つからなくなったら停止してクッションページ用URLを適用
+		return self::make_jump_url($url);
+		
+	}
+	
+	//URLクッションページ用
+	function make_jump_url($url) {
+		
+		$display_url = mb_strimwidth($url,0,35,"...");
+		$enc_url = rawurlencode(base64_encode($url));
+		$back = rawurlencode(base64_encode(getenv('REQUEST_URI')));
+		return "<a href=\"jump.php?guid=ON&back={$back}&url={$enc_url}\">{$display_url}</a>";
+		
+	}
+	
+	//テキストを解析して成分ごとに分割して配列に格納
+	function __toArray($text) {
+		
+		//配列を初期化
+		$array[0]['str'] = $text;
+		$array[0]['type'] = false;
+		
+		/*URLで分割*/
+		
+		//gTLD
+		$gTLD = array("aero","arpa","asia","biz","cat","com","coop","edu","gov","info","int","jobs","mil","mobi","museum","name","net","org","pro","tel","travel");
+		
+		//ccTLD
+		$ccTLD = array("ac","ad","ae","af","ag","ai","al","am","an","ao","aq","ar","as","at","au","aw","ax","az","ba","bb","bd","be","bf","bg","bh","bi","bj","bm",
+				"bn","bo","br","bs","bt","bv","bw","by","bz","ca","cc","cd","cf","cg","ch","ci","ck","cl","cm","cn","co","cr","cs","cu","cv","cx","cy","cz",
+				"dd","de","dj","dk","dm","do","dz","ec","ee","eg","eh","er","es","et","eu","fi","fj","fk","fm","fo","fr","ga","gb","gd","ge","gf","gg","gh",
+				"gi","gl","gm","gn","gp","gq","gr","gs","gt","gu","gw","gy","hk","hm","hn","hr","ht","hu","id","ie","il","im","in","io","iq","ir","is","it",
+				"je","jm","jo","jp","ke","kg","kh","ki","km","kn","kp","kr","kw","ky","kz","la","lb","lc","li","lk","lr","ls","lt","lu","lv","ly","ma","mc",
+				"md","me","mg","mh","mk","ml","mm","mn","mo","mp","mq","mr","ms","mt","mu","mv","mw","mx","my","mz","na","nc","ne","nf","ng","ni","nl","no",
+				"np","nr","nu","nz","om","pa","pf","pg","ph","pk","pl","pm","pn","pr","ps","pt","pw","py","qa","re","ro","rs","ru","rw","sa","sb","sc","sd",
+				"se","sg","sh","si","sj","sk","sl","sm","sn","so","sr","st","su","sv","sy","sz","tc","td","tf","tg","th","tj","tk","tl","tm","tn","to","tp",
+				"tr","tt","tv","tw","tz","ua","ug","uk","um","us","uy","uz","va","vc","ve","vg","vi","vn","vu","wf","ws","ye","yt","yu","za","zm","zr","zw");
+		
+		//パターン結合
+		$pattern1 = "https?:\/\/([A-Za-z0-9]+\.)+(".implode("|",array_merge($gTLD,$ccTLD)).")(\/[^<>\"(){}\\^[\]`。、，”□△◎☆！？～＠\s　]*)?";
+		$pattern2 = "([A-Za-z0-9]+\.)+(".implode("|",$gTLD).")(\/[^<>\"(){}\\^[\]`。、，”□△◎☆！？～＠\s　]*)?";
+		$pattern3 = "([A-Za-z0-9]+\.){2,}(".implode("|",$ccTLD).")(\/[^<>\"(){}\\^[\]`。、，”□△◎☆！？～＠\s　]*)?";
+		$pattern = "/({$pattern1})|({$pattern2})|({$pattern3})/us";
+		
+		//URLを見つけて配列に区切って入れていく
+		while (true) {
+		
+			$temp_arr = array_pop($array);
+			
+			if (preg_match($pattern,$temp_arr['str'],$matches)) {
+			
+				$url_pos = mb_strpos($temp_arr['str'],$matches[0]);
+				$url_len = mb_strlen($matches[0]);
+				$url_str = $matches[0];
+				$array[] = array('str'=>mb_substr($temp_arr['str'],0,$url_pos),'type'=>false);
+				$array[] = array('str'=>$url_str,'type'=>'url');
+				$array[] = array('str'=>mb_substr($temp_arr['str'],$url_pos+$url_len),'type'=>false);
+				continue;
+				
+			}
+			
+			$array[] = $temp_arr;
+			break;
+			
+		}
+		
+		//前後関係からURLを再判定
+		$last_char = "";
+		
+		foreach ($array as &$pair) {
+		
+			if ($pair['type']!='url') {
+			
+				$last_char = mb_substr($pair['str'],mb_strlen($pair['str'])-1);
+				
+			} else {
+			
+				if (preg_match("/[A-Za-z0-9]/u",$last_char)) $pair['type'] = false;
+				$last_char = "";
+				
+			}
+		}
+		
+		/*ハッシュタグ・英単語で分割*/
+		
+		//パターン
+		$pattern = "/(@[A-Za-z0-9_]{1,15})|([#♯][ー゛゜々ヾヽぁ-ヶ一-龠ａ-ｚＡ-Ｚ０-９a-zA-Z0-9_]{1,139})|([A-Za-z0-9\-_.,:;]{1,140})/us";
+		
+		//実際に分割
+		$cnt=0;
+		
+		while (true) {
+		
+			$temp_arr = $array[$cnt];
+			
+			if ($temp_arr===NULL) break;
+			
+			//無属性は無視
+			if ($temp_arr['type']!==false) {
+			
+				$cnt++;
+				continue;
+				
+			}
+			
+			if (preg_match($pattern,$temp_arr['str'],$matches)) {
+			
+				$pos = mb_strpos($temp_arr['str'],$matches[0]);
+				$len = mb_strlen($matches[0]);
+				$str = $matches[0];
+				
+				//種類を判定
+				if ($matches[1]) $type = 'screen_name';
+				elseif ($matches[2]) $type = 'hashtag';
+				else $type = 'word';
+				
+				array_splice($array,$cnt,1,
+					array(
+						array('str'=>mb_substr($temp_arr['str'],0,$pos),'type'=>false),
+						array('str'=>$str,'type'=>$type),
+						array('str'=>mb_substr($temp_arr['str'],$pos+$len),'type'=>false)
+					)
+				);
+				$cnt += 2;
+				continue;
+				
+			}
+			
+			$cnt++;
+				
+		}
+		
+		//配列を返す
+		return $array;
+		
 	}
 
 }
